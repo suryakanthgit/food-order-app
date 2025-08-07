0f39e12efb1c248911fba2343feeb18df9063991
dilse <dilse@DESKTOP-OJ6AD86>
2025-08-07 07:59:07 +0530

    Fix: corrected _name_ and _main_ syntax

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def order():
 
     return render_template("order.html", ordered_items=ordered_items, total=total)
 
-if _name_ == '_main_':
+if __name__ == '__main__git add app.py
     import os
     port = int(os.environ.get("PORT", 5000))  # Render provides PORT env variable
     app.run(host='0.0.0.0', port=port)
